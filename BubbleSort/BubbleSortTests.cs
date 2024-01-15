8c9bc93d65885beccd56b8a0542d0d744046fca8
Zeds_Xd <homenkor24@gmail.com>
2024-01-15 12:01:16 +0300

    Bubble Sort

---
+++ BubbleSort/BubbleSortTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            var bubble = new BubbleSort<int>();
+            var random = new Random();
+            var items = new List<int>();
+
+            for (int index = 0; index < 800; index++)
+            {
+                items.Add(random.Next(0, 100));
+            }
+
+            bubble.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();
+
+            bubble.Sort();
+
+            for (int index = 0; index < items.Count; index++)
+            {
+                Assert.AreEqual(sorted[index], bubble.Items[index]);
+            }
+        }
+    }
+}
