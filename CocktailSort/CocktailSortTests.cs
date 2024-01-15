7376bf9f2f81d8df99b2aab1a2bd82835799f424
Zeds_Xd <homenkor24@gmail.com>
2024-01-15 13:06:13 +0300

    CoctailSort

---
+++ CocktailSort/CocktailSortTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Algorithm;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class CocktailSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            var cocktail = new CocktailSort<int>();
+
+            var random = new Random();
+            var items = new List<int>();
+
+            for (int index = 0; index < 10; index++)
+            {
+                items.Add(random.Next(0, 100));
+            }
+
+            cocktail.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();
+
+            cocktail.Sort();
+
+            for (int index = 0; index < items.Count; index++)
+            {
+                Assert.AreEqual(sorted[index], cocktail.Items[index]);
+            }
+        }
+    }
+}
