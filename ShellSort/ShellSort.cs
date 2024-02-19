2e2a71cbf80a3908f9c6e4842666d1e8b21950dd
Zeds_Xd <homenkor24@gmail.com>
2024-02-19 17:20:22 +0300

    Add new Sort: Shell Sort

---
+++ ShellSort/ShellSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class ShellSort<T>: AlgorithmBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            var step = Items.Count / 2;
+
+            while (step > 0)
+            {
+                for (int i = step; i < Items.Count; i++)
+                {
+                    int j = i;
+                    
+                    while ((j >= step) && Compare(Items[j - step], Items[j]) == 1) 
+                    {
+                        Swop(j- step, j);
+                        j -= step;
+                    }
+                }
+
+                step /= 2;
+            }
+        }
+    }
+}
