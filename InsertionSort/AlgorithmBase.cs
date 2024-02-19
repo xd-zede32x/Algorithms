9fc182c202f221cd874919d85952c1f9d4a38336
Zeds_Xd <homenkor24@gmail.com>
2024-02-19 11:39:36 +0300

    Add new Algorithm InsertionSort

---
+++ InsertionSort/AlgorithmBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class AlgorithmBase<T> where T : IComparable
+    {
+        public int SwopCount { get; protected set; } = 0;
+        public int ComparisonCount { get; protected set; } = 0;
+
+        public List<T> Items { get; set; } = new List<T>();
+
+        protected void Swop(int positionA, int positionB)
+        {
+            if (positionA < Items.Count && positionB < Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+
+                SwopCount++;
+            }
+        }
+
+        public TimeSpan Sort()
+        {
+            var timer = new Stopwatch();
+            SwopCount = 0;
+            timer.Start();
+
+            MakeSort();
+
+            Items.Sort();
+            timer.Stop();
+
+            return timer.Elapsed;
+        }
+
+        protected virtual void MakeSort() => Items.Sort();
+    }
+}
---
+++ InsertionSort/SortTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class SortTests
+    {
+        Random random = new Random();
+
+        List<int> Items = new List<int>();
+        List<int> Sorted = new List<int>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            Items.Clear();
+
+            for (int index = 0; index < 10000; index++)
+                Items.Add(random.Next(0, 1000));
+
+            Sorted.Clear();
+            Sorted.AddRange(Items.OrderBy(x => x).ToArray());
+        }
+
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            var bubble = new BubbleSort<int>();
+            bubble.Items.AddRange(Items);
+            bubble.Sort();
+
+            for (int index = 0; index < Items.Count; index++)
+                Assert.AreEqual(Sorted[index], bubble.Items[index]);
+        }
+
+        [TestMethod]
+        public void CocktailSortTest()
+        {
+            var cocktail = new CocktailSort<int>();
+            cocktail.Items.AddRange(Items);
+            cocktail.Sort();
+
+            for (int index = 0; index < Items.Count; index++)
+                Assert.AreEqual(Sorted[index], cocktail.Items[index]);
+        }
+
+        [TestMethod]
+        public void InsertionSortTest()
+        {
+            var insert = new InsertionSort<int>();
+            insert.Items.AddRange(Items);
+            insert.Sort();
+
+            for (int index = 0; index < Items.Count; index++)
+                Assert.AreEqual(Sorted[index], insert.Items[index]);
+        }
+    }
+}
