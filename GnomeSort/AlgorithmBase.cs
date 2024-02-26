789a7eddb945494188eb9832360c3b75535f5342
Zeds_Xd <homenkor24@gmail.com>
2024-02-26 20:20:59 +0300

    Add new Algorithm: GnomeSort

---
+++ GnomeSort/AlgorithmBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class AlgorithmBase<T> where T : IComparable
+    {
+        public int SwopCount { get; protected set; } = 0;
+        public int ComparisonCount { get; protected set; } = 0;
+
+        public List<T> Items { get; set; } = new List<T>();
+
+        public event EventHandler<Tuple<T, T>> SwopEvent;
+        public event EventHandler<Tuple<T, T>> CompareEvent;
+
+        public AlgorithmBase() { }
+        public AlgorithmBase(IEnumerable<T> items) => Items.AddRange(items);
+
+        protected void Swop(int positionA, int positionB)
+        {
+            if (positionA < Items.Count && positionB < Items.Count)
+            {
+                SwopEvent?.Invoke(this, new Tuple<T, T>(Items[positionA], Items[positionB]));
+                SwopCount++;    
+
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+
+        public TimeSpan Sort()
+        {
+            var timer = new Stopwatch();
+            SwopCount = 0;
+            timer.Start();
+
+            MakeSort();
+
+            Items.Sort();
+            timer.Stop();
+
+            return timer.Elapsed;
+        }
+
+        protected virtual void MakeSort() => Items.Sort();
+
+        public int Compare(T a, T b)
+        {
+            CompareEvent?.Invoke(this, new Tuple<T, T>(a, b));
+            ComparisonCount++;
+            return a.CompareTo(b);
+        }
+    }
+}
---
+++ GnomeSort/GnomeSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class GnomeSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public GnomeSort() { }
+        public GnomeSort(IEnumerable<T> items) : base(items) { }
+
+        protected override void MakeSort()
+        {
+            var index = 1;
+
+            while (index < Items.Count)
+            {
+                if (index == 0 || Compare(Items[index], Items[index - 1]) == 1)
+                    index++;
+
+                else
+                {
+                    Swop(index, index - 1);
+                    index--;
+                }   
+            }
+        }
+    }
+}
