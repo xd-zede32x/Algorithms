4ecc81e9282a1ca9d190be3a47653aba403ec182
Zeds_Xd <homenkor24@gmail.com>
2024-02-26 13:11:38 +0300

    Add new Algorihtm: Selection Sort

---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class SelectionSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public SelectionSort() { }
+        public SelectionSort(IEnumerable<T> items) : base(items) { }
+
+        protected override void MakeSort()
+        {
+            var minIndex = 0;
+
+            for (int i = 0; i < Items.Count - 1; i++)
+            {
+                minIndex = i;
+
+                for (int j = i + 1; j < Items.Count; j++)
+                {
+                    if (Compare(Items[j], Items[minIndex]) == -1)
+                        minIndex = j;
+                }
+
+                if (i != minIndex)
+                    Swop(i, minIndex);
+            }
+        }
+    }
+}
